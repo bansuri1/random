c9aed55ac9747abd962807aefc416d41fb6712e3
bansuri1 <89803987+bansuri1@users.noreply.github.com>
2021-10-22 22:34:40 -0400

    Add files via upload

---
+++ open vine.py
@@ -0,0 +1,28 @@
+import pyautogui as pag
+import time
+
+# note: browser window should be open somewhere on the left side of the screen! :)
+
+pag.moveTo(5,1079,2)
+pag.click()
+time.sleep(1)
+
+pag.write('Chrome',0.1)
+pag.hotkey('enter')
+
+time.sleep(1) 
+pag.moveTo(1674,1053,1)
+pag.click()
+
+time.sleep(1)
+pag.moveTo(1693,984,2)
+pag.click()
+
+time.sleep(2) 
+pag.moveTo(725,125,2)
+pag.click()
+
+time.sleep(2) 
+pag.moveTo(700,700,2)
+pag.click()
+
